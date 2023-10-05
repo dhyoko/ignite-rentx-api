bc2bebc14642f9a5c5d35fb41560acdf50bd859c
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-10-05 15:24:41 -0300

    Add user repository

---
+++ src/modules/accounts/repositories/implementation/UserRepository.ts
@@ -0,0 +1,28 @@
+import { Repository } from "typeorm";
+
+import dataSource from "../../../../database";
+import { User } from "../../entities/User";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+  async create(data: ICreateUserDTO): Promise<void> {
+    const { name, email, password, driver_license } = data;
+
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
