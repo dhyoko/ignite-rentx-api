24b574107dd57ce2fb947c0f3190638afccf3b3a
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-10-05 15:41:51 -0300

    Add password hashing

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../repositories/dtos/ICreateUserDTO";
@@ -12,10 +13,18 @@ class CreateUserUseCase {
 
   async execute(data: ICreateUserDTO): Promise<void> {
     const { name, email, password, driver_license } = data;
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
