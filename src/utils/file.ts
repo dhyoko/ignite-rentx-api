0b107ffddf2d47b45e2296ae7906db257fbc2f38
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-10-06 10:19:20 -0300

    Add file and upload handlers

---
+++ src/utils/file.ts
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+const deleteFile = async (filename: string): Promise<void> => {
+  try {
+    await fs.promises.stat(filename);
+  } catch {
+    return;
+  }
+  await fs.promises.unlink(filename);
+};
+
+export { deleteFile };
