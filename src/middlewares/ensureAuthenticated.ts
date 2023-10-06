8ddc87807b345ab176e85516c34b03440b9345ce
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-10-06 10:19:37 -0300

    Add avatar controller

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -31,6 +31,10 @@ const ensureAuthenticated = async (
       throw new AppError("User does not exists", 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch {
     throw new AppError("Invalid token", 401);
