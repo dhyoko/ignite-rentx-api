af62cb5c81873fafe8af4257ef49d01c91d24f31
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-10-11 09:25:17 -0300

    Fix data source entities path

--- src/shared/infra/typeorm/data-source.config.ts
+++ src/shared/infra/typeorm/data-source.config.ts
@@ -1,10 +1,6 @@
 import dotenv from "dotenv";
 import { DataSourceOptions } from "typeorm";
 
-import { User } from "@modules/accounts/infra/typeorm/entities/User";
-import { Category } from "@modules/cars/infra/typeorm/entities/Category";
-import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
-
 dotenv.config();
 
 interface IConfig {
@@ -22,6 +18,6 @@ export function getConfig({ isMigration }: IConfig): DataSourceOptions {
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
     migrations: ["./src/shared/infra;typeorm/migrations/*.{ts,js}"],
-    entities: [Category, Specification, User],
+    entities: ["./src/modules/**/entities/*.ts"],
   } as DataSourceOptions;
 }
